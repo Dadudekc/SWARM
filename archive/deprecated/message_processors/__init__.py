a865dc07f377cf5de95d3fc2bf22b0d526b5d004
Victor Dixon <135445391+Dadudekc@users.noreply.github.com>
2025-06-07 12:14:10 -0500

    Remove deprecated archive directory

--- archive/deprecated/message_processors/__init__.py
+++
@@ -1,8 +0,0 @@
-# AUTO-GENERATED __init__.py
-# DO NOT EDIT MANUALLY - changes may be overwritten
-
-from . import legacy_message_processor
-
-__all__ = [
-    'legacy_message_processor',
-]
--- archive/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# AUTO-GENERATED __init__.py
-# DO NOT EDIT MANUALLY - changes may be overwritten
-
-from . import error_management_legacy_tests
-
-__all__ = [
-    'error_management_legacy_tests',
-]
