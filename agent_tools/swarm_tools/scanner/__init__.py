2f6afdec50048ccacfbe9aec154d1497c6a8f9f1
DaDudekC <dadudekc@gmail.com>
2025-06-08 22:55:02 -0500

    fix(scanner): Update module exports

--- agent_tools/swarm_tools/scanner/__init__.py
+++ agent_tools/swarm_tools/scanner/__init__.py
@@ -1,9 +1,21 @@
 """Scanner package for project analysis."""
 
 from .scanner import Scanner
-from .dry_scanner import DryScanner
+from .dry_scanner import (
+    CodeHasher,
+    CodeNormalizer,
+    ASTAnalyzer,
+    TestAnalyzer,
+    CodeLocation,
+    DuplicateGroup
+)
 
 __all__ = [
     'Scanner',
-    'DryScanner',
+    'CodeHasher',
+    'CodeNormalizer',
+    'ASTAnalyzer',
+    'TestAnalyzer',
+    'CodeLocation',
+    'DuplicateGroup'
 ]
