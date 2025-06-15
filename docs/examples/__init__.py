b3ac57514281466645ddc09aaeaf84113b11ae7f
Victor Dixon <135445391+Dadudekc@users.noreply.github.com>
2025-06-15 09:31:17 -0500

    Consolidate example scripts and clean duplicates

--- docs/examples/__init__.py
+++ docs/examples/__init__.py
@@ -2,7 +2,9 @@
 # DO NOT EDIT MANUALLY - changes may be overwritten
 
 from . import llm_agent_example
+from . import messaging_example
 
 __all__ = [
     'llm_agent_example',
+    'messaging_example',
 ]
--- dreamos/core/ai/gpt_router/profiles/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Package profiles."""
-
-__version__ = "0.1.0"
